630b8c4d9620578699a7d7026222c791d0f296bf
Lydia Tan <lydia.ly.tan@gmail.com>
2021-01-12 12:57:18 -0500

    added comments to Joke.js noting current issue in vscode

--- src/components/Joke.js
+++ src/components/Joke.js
@@ -2,6 +2,8 @@ import React from "react";
 //joke creates components that displays the questions from database
 function Joke(props){
     // console.log(props);
+    //this is another unnecessary edit just so that I can test and practice this commit --> something is a little funky in visual studio so i will be working on trying to commit exclusively through the terminal
+    // the version control section of vscode right now doesn't seem to be updating or allowing me to push changes somehow? so I will need to do it from the terminal 
     
     return(
         <div>

3c9d1014ae0211be429cdad2d05ca9b58254f829
Lydia Tan <lydia.ly.tan@gmail.com>
2021-01-12 12:53:17 -0500

    testing branch push

--- src/components/Products.js
+++ src/components/Products.js
@@ -3,7 +3,8 @@ import products from "./ProductsData";
 
 // here is where I try to pass in the values from the products data file and then attempt to pass each of those properties into an html element (maybe like product cards, etc)
 
-//I'm essentially trying to list out each of the unique products and their individual properties 
+//I'm essentially trying to list out each of the unique products and their individual properties
+//testing updates and editing comment unnecessarily
 
 function Products(props){
     //incorrect --> this stuff that I'm trying to do is meant to be executed in the App.js file, not here

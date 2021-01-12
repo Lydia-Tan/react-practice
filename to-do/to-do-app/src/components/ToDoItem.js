aa34b1bf273b415643c36f6998f83be5fe37a826
Lydia Tan <lydia.ly.tan@gmail.com>
2021-01-12 15:42:08 -0500

    checkboxes take the boolean value from completed obj attr

--- to-do/to-do-app/src/components/ToDoItem.js
+++ to-do/to-do-app/src/components/ToDoItem.js
@@ -3,7 +3,7 @@ import React from "react";
 function ToDoItem(props){
     return(
         <div>
-            <input type = "checkbox" />
+            <input type = "checkbox" checked = {props.itemDo.completed} />
             <label>{props.itemDo.task}</label>
             <br/>
             <br/>

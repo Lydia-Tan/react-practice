15fd21f6b796dc84244cfa92b58007cdaeb63b91
Lydia Tan <lydia.ly.tan@gmail.com>
2021-01-12 16:00:44 -0500

    created class based component, added tester method + display

--- src/App.js
+++ src/App.js
@@ -12,55 +12,75 @@ import jokesData from "./components/JokesData";
 import Products from "./components/Products.js";
 import productsData from "./components/ProductsData.js";
 
-function App() {
-  const allProds = productsData.map(prod =>
-    //first variation of implementing:
-    // <Products key={prod.id} name={prod.name} price={prod.price} description={prod.description}/>
-    
-    //second variation of implementing:
-    <Products key={prod.id} item={prod}/>
 
-  ) 
-
-  //because map returns a new array, we are creating a variable jokecomponent which stores the new array
-  const jokeComponents = jokesData.map(joke => 
-      <Joke key={joke.id} question={joke.question} punchLine={joke.punchLine}/>
-  )
-
-  return (
-  
-    <div className="App">
-      <Header />
-      <TimeGreeting />
-
+//the use of extends essentially allows for us to use all of the React.Component "goodies" to be used in this class
+class App extends React.Component{
+  yourMethod(){
+    return(
+    <h2>THIS IS SO COOL</h2>
+    )
+  }
+  //needs at least one method (render() method)
+  render(){
+    const tester = this.yourMethod();
+    return(
       <div>
-        {allProds}
+        {tester}
+        <AboutMe/>
       </div> 
-
-      <div>
-        {jokeComponents}
-      </div>
-
-      {/* <Joke 
-        answer="It's hard to explain puns to kleptomaniacs because they always take things literally."   
-      />
-
-      <Joke 
-        question="What's the best thing about Switzerland?"
-        answer="I don't know, but the flag is a big plus!"   
-      />
-
-      <Joke 
-        question="Give me a joke?"
-        answer="You"   
-      /> */}
-
-      <AboutMe />
-      <ToDo />
-      <Footer />
-
-    </div>
-  );
+    )
+  }
 }
+
+// function App() {
+//   const allProds = productsData.map(prod =>
+//     //first variation of implementing:
+//     // <Products key={prod.id} name={prod.name} price={prod.price} description={prod.description}/>
+    
+//     //second variation of implementing:
+//     <Products key={prod.id} item={prod}/>
+
+//   ) 
+
+//   //because map returns a new array, we are creating a variable jokecomponent which stores the new array
+//   const jokeComponents = jokesData.map(joke => 
+//       <Joke key={joke.id} question={joke.question} punchLine={joke.punchLine}/>
+//   )
+
+//   return (
+  
+//     <div className="App">
+//       <Header />
+//       <TimeGreeting />
+
+//       <div>
+//         {allProds}
+//       </div> 
+
+//       <div>
+//         {jokeComponents}
+//       </div>
+
+//        <Joke 
+//         answer="It's hard to explain puns to kleptomaniacs because they always take things literally."   
+//       />
+
+//       <Joke 
+//         question="What's the best thing about Switzerland?"
+//         answer="I don't know, but the flag is a big plus!"   
+//       />
+
+//       <Joke 
+//         question="Give me a joke?"
+//         answer="You"   
+//       /> 
+
+//       <AboutMe />
+//       <ToDo />
+//       <Footer />
+
+//     </div>
+//   );
+// } 
 
 export default App

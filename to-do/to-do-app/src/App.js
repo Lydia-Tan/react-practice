c378c6f53ea4a2388f2d2d178bcd46abad6ab2c1
Lydia Tan <lydia.sydia@gmail.com>
2021-01-14 22:53:56 -0500

    updated todo app so that task items are in the state

--- to-do/to-do-app/src/App.js
+++ to-do/to-do-app/src/App.js
@@ -3,15 +3,34 @@ import React from "react";
 import ToDoItem from "./components/ToDoItem";
 import toDoData from "./components/ToDoData";
 
-function App() {
-  const toDoComponent = toDoData.map(taskItem =>
-      <ToDoItem key={taskItem.id} itemDo={taskItem}/>  
-  )
-  return (
-    <div className="App">
-      {toDoComponent}
+
+class App extends React.Component{
+  constructor(){
+    super()
+    let toDoItem = toDoData.map(taskItem => 
+    <ToDoItem key = {taskItem.id} itemDo = {taskItem}/>
+    )
+    this.state = {
+      taskList: toDoItem 
+    }
+  }
+
+  render(){
+    return <div className = "App">
+      {this.state.taskList}
     </div>
-  );
+  }
 }
+// function App() {
+//   const toDoComponent = toDoData.map(
+//     taskItem =>
+//     <ToDoItem key={taskItem.id} itemDo={taskItem}/>  
+//   )
+//   return (
+//     <div className="App">
+//       {toDoComponent}
+//     </div>
+//   );
+// }
 
 export default App;

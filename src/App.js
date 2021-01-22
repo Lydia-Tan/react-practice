19ab9af1667ce6d510ecb4658579e24abc12ebb9
Lydia Tan <lydia.sydia@gmail.com>
2021-01-21 22:57:20 -0500

    created loading state when fetching api data

--- src/App.js
+++ src/App.js
@@ -5,25 +5,32 @@ class App extends React.Component{
   constructor(){
     super();
     this.state = {
+      loading: false,
       character: {}
     }
     // this.handleClick = this.handleClick.bind(this);
   } 
   
   componentDidMount(){
+    this.setState({
+      loading:true
+    })
     fetch("https://swapi.dev/api/people/1")
       .then(response => response.json())
       .then(data => {
         this.setState({
-          character:data
+          loading: false,
+          character: data
         })
       })
   }
 
   render(){
+    const text = this.state.loading ? "loading..." : this.state.character.name
     return(
       <div>
-        <h1>{this.state.character.name}</h1>
+        
+        <h1>{text}</h1>
 
       </div>
     )
